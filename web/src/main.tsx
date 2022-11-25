0a03884bc9c8f957a2cc91984bb40424d0bad9a2
emanuelklado <emanuelklado@msn.com>
2022-11-25 16:53:54 -0300

    fix: ajustes de rotas com browser router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AdsDetails from './pages/AdsDetails'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/ads/:id' element={<AdsDetails />} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
