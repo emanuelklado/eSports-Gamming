eba409622656c963c6265e265e9d81a4c5237a6e
emanuelklado <emanuelklado@msn.com>
2022-12-01 00:43:14 -0300

    mock: feito mock de lista de avatares para renderizarem de maneira aleatoria enquanto não temos upload do avatar

---
+++ web/src/helpers/arrayOfAvatars.ts
@@ -0,0 +1,9 @@
+// array de avatar
+
+export const arrayOfAvatars = [
+    'https://cdn-icons-png.flaticon.com/512/147/147133.png',
+    'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+    'https://cdn-icons-png.flaticon.com/512/147/147142.png',
+    'https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png',
+    'https://cdn-icons-png.flaticon.com/512/219/219969.png'
+]
