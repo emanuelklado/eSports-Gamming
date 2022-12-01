d23befdb1391899408d407321ea0c50e792a673a
emanuelklado <emanuelklado@msn.com>
2022-12-01 00:43:49 -0300

    cleaning: deletando arquivos nao usados

--- web/src/helpers/numbersToDaysOfWeek.ts
+++
@@ -1,7 +0,0 @@
-// função para transformar um array de numero de 0 a 6 em um array de dias da semana
-export default function numbersToDaysOfWeek(days: number[]) {
-    const daysOfWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-    return days.map(day => (
-        `${daysOfWeek[day]}`
-    ));
-}
